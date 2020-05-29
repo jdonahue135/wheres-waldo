4ad09332cc7ef55eccd9dcdae7b1aee2e43fe4f9
Jake Donahue <jdonahue135@gmail.com>
2020-05-29 12:42:44 -0400

    add styles and formatTime helper function

--- client/src/components/LeaderBoard.js
+++ client/src/components/LeaderBoard.js
@@ -1,15 +1,21 @@
 import React from "react";
 
+import { formatTime } from "../helpers";
+
 const LeaderBoard = (props) => {
   return (
-    <div>
-      <div>Leader Board</div>
-      {props.scores.map((score) => (
-        <div key={score.name + score.seconds}>
-          {score.name + " " + score.seconds}
-        </div>
-      ))}
-      <div onClick={props.onClick}>Try Again</div>
+    <div className="leaderboard">
+      <div>Leaderboard</div>
+      <ol>
+        {props.scores.map((score) => (
+          <li key={score.name + score.seconds}>
+            {score.name + "    " + formatTime(score.seconds)}
+          </li>
+        ))}
+      </ol>
+      <div className="link" onClick={props.onClick}>
+        Try Again
+      </div>
     </div>
   );
 };
