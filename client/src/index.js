700ebaf1a145e52463c7a5341ac81c6e2baf408f
Jake Donahue <jdonahue135@gmail.com>
2020-05-29 12:47:43 -0400

    reorganize directory structure, rename css file and delete redundant files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./styles/App.css";
+import "./styles/styles.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
