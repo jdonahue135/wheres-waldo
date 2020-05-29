86de91d45d49cd3ae6e4f16759740170b63ea6d4
Jake Donahue <jdonahue135@gmail.com>
2020-05-29 12:43:18 -0400

    add promopt content when user does not get a high score

--- client/src/components/Prompt.js
+++ client/src/components/Prompt.js
@@ -1,17 +1,30 @@
 import React from "react";
 
 const Prompt = (props) => {
-  return props.show ? (
-    <div className="prompt">
-      <p>High Score! Enter your Name:</p>
-      <input type="text" onChange={props.onChange} />
-      <button type="button" onClick={props.onSubmit}>
-        Submit
-      </button>
-    </div>
-  ) : (
-    <div></div>
-  );
+  if (props.show) {
+    if (props.highScore) {
+      return (
+        <div className="prompt">
+          <p>High Score! Enter your Name:</p>
+          <input type="text" onChange={props.onChange} />
+          <button type="button" onClick={props.onSubmit}>
+            Submit
+          </button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="prompt">
+          <p>Too slow, no high Score</p>
+          <div className="link" onClick={props.onClick}>
+            View Leaderboard
+          </div>
+        </div>
+      );
+    }
+  } else {
+    return <div></div>;
+  }
 };
 
 export default Prompt;
