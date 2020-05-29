b424d342e568a389fb371442b84bdcca6d2fb5b9
Jake Donahue <jdonahue135@gmail.com>
2020-05-29 13:55:12 -0400

    fix heroku deployment issue with api calls returning static html page

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "client/build")));
-app.get("*", (req, res) => {
+/*app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
-});
+});*/
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
